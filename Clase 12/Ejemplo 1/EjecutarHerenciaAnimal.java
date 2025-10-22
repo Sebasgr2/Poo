50b7f1ad353b9ebe6f9a37f1bf3502e59e7ec1eb
Sebastian Gonzalez Reyes <sebastian.gonzale@campusucc.edu.co>
2025-10-22 20:48:57 +0000

    Herencia Java

---
+++ Clase 12/Ejemplo 1/EjecutarHerenciaAnimal.java
@@ -0,0 +1,11 @@
+public class EjecutarHerenciaAnimal {
+
+    public static void main(String[] args) {
+        Animal Hades = new Perro();
+        Animal Gojo = new Gato();
+
+
+        Gojo.HacerSonido();
+        Hades.HacerSonido();
+    }
+}
